d1ef8d0c871c916add411af41b3d5a24bd978fb0
Дмитрий Морозов <ADM713934@yandex.ru>
2023-07-27 20:08:26 +0300

    migrations added

--- yacut/error_handlers.py
+++ yacut/error_handlers.py
@@ -1,3 +1,5 @@
+from http import HTTPStatus
+
 from flask import jsonify, render_template
 
 from . import app
@@ -24,7 +26,7 @@ def invalid_api_usage(error):
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('404.html'), 404
+    return render_template('404.html'), HTTPStatus.NOT_FOUND
 
 
 def check_unique_short_url(custom_id):
